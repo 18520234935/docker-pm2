dd7847b9990f37420bafebf3ad516cb1376d0fbd
alex <939919096@qq.com>
2020-11-17 22:27:05 +0800

    docker

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+
 const http = require('http')
-const createHandler = require('github-webhook-handler') //这个插件使用前先安装 npm i github-webhook-handler -D
+const createHandler = require('github-webhook-handler')
 
 function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
   var child = spawn(cmd, args);
-  var resp = "";
+  let  resp = "";
   child.stdout.on('data', function (buffer) {
     resp += buffer.toString();
   });
   child.stdout.on('end', function () {
-    console.log('Deploy 完成')
-    
+    console.log('Deploy 完成');
+
     callback(resp)
   });
 }
 
 const handler = createHandler({
-    path:'/source/pm2', // url 后缀
+    path:'/pm2', // url 后缀
     secret:'123123' // 你的密码
 })
 
@@ -26,7 +27,7 @@ http.createServer((req,res) => {
     res.end('no such location')
   })
 }).listen(7777, () => {
-  console.log('Webhook listen at 9090')
+  console.log('Webhook listen at 7777')
 })
 
 handler.on('error',err => {
@@ -34,11 +35,12 @@ handler.on('error',err => {
 })
 
 // 拦截push，执行 Deploy 脚本
-handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
-  // 分支判断
-  if(event.payload.ref === 'refs/heads/master'){
-    console.log('deploy master..')
-    run_cmd('sh', ['./autoDepoly.sh'], function(text){ console.log(text) });
-  }
-})
+handler.on('push', (event)=> {
+  console.log('Received push');
+    if(event.payload.ref === 'refs/heads/init'){
+
+    run_cmd('sh', ['./autoDepoly.sh'], function(text){
+        console.log(text)
+        })
+    }
+ });
